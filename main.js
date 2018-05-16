46c3465548ee8a58eb552beeaaa2bcf4ac68b03a
Faithful-Mind <lpyy15@gmail.com>
2018-05-16 20:00:17 +0800

    Cat Clicker Duo with 2 cats and its names

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var i = 0;
-document.querySelector('span.number').textContent = i
+var num1 = 0;
+document.querySelector('.cat-1 span.number').textContent = num1;
+document.querySelector('.cat-1 figcaption').textContent = 'Dragon Li';
+document.querySelector('.cat-1 img.cat-pic').addEventListener('click', function() {
+  num1++;
+  document.querySelector('.cat-1 span.number').textContent = num1;
+});
 
-document.querySelector('img.cat-pic').addEventListener('click', function() {
-  i++;
-  document.querySelector('span.number').textContent = i;
-})
+var num2 = 0;
+document.querySelector('.cat-2 span.number').textContent = num2;
+document.querySelector('.cat-2 figcaption').textContent = 'Calico cat';
+document.querySelector('.cat-2 img.cat-pic').addEventListener('click', function() {
+  num2++;
+  document.querySelector('.cat-2 span.number').textContent = num2;
+});
